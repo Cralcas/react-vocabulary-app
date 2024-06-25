6bc0485c8c99bd66636f5acf3646b4072d2e32f2
Cralcas <carlosbergq@gmail.com>
2024-06-26 00:25:43 +0200

    localStorage for courses ✨

--- src/hooks/useCourse.tsx
+++ src/hooks/useCourse.tsx
@@ -3,11 +3,18 @@ import {Course} from "../models/Course";
 import {IWord} from "../models/IWord";
 
 export const useCourse = () => {
-  const [courses, setCourses] = useState<Course[]>([]);
+  const getSavedCourses = (): Course[] => {
+    const savedCourses = localStorage.getItem("courses");
+    return savedCourses ? JSON.parse(savedCourses) : [];
+  };
+
+  const [courses, setCourses] = useState<Course[]>(getSavedCourses);
 
   const addCourse = (genre: string, language: string, words: IWord[]) => {
     const newCourse = new Course(genre, language, words);
-    setCourses([...courses, newCourse]);
+    const updatedCourses = [...courses, newCourse];
+    setCourses(updatedCourses);
+    localStorage.setItem("courses", JSON.stringify(updatedCourses));
   };
 
   return {courses, addCourse};
