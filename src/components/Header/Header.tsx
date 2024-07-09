ce3ab5f711efd1ac952d5950a1fe08b1898550af
Cralcas <carlosbergq@gmail.com>
2024-07-09 23:20:17 +0200

    layout + card styling 💄

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,14 @@
 import { NavLink } from "react-router-dom";
 import "./Header.scss";
-import { useEffect, useState } from "react";
 
 export const Header = () => {
-  const [hasScrolled, setHasScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setHasScrolled(true);
-      } else {
-        setHasScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
-    <>
-      <header className={`header ${hasScrolled ? "shadow" : ""}`}>
-        <nav>
-          <NavLink to={"/"}>
-            <img src="/images/book-study.svg" />
-          </NavLink>
-        </nav>
-      </header>
-    </>
+    <header>
+      <nav>
+        <NavLink to={"/"}>
+          <img src="/images/book-study.svg" />
+        </NavLink>
+      </nav>
+    </header>
   );
 };
